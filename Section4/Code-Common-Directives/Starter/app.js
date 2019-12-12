ff1a67690e527e23802aa244068b8d957aeaad3c
Scott Rizzo <scottrizzo@Scotts-MacBook-Pro-2.local>
2019-12-12 14:43:11 -0800

    common directives

--- Section4/Code-Common-Directives/Starter/app.js
+++ Section4/Code-Common-Directives/Starter/app.js
@@ -8,4 +8,16 @@ myApp.controller('mainController', ['$scope', '$filter', function($scope, $filte
         return $filter('lowercase')($scope.handle);
     };
     
+    $scope.characters = 5;
+    $scope.rules = [
+        {ruleName: "Must be 5 characters"},
+        {ruleName: "Must not be used else where"},
+        {ruleName: "It must be cool"}
+    ];
+    
+    $scope.alertClick = function() {
+        alert("Button Clicked");
+    }
+    
+    $scope.name = 'John Doe';
 }]);
